3cc76433520055289f6951492f2ab57a03d4e341
daphay <dafizo1@gmail.com>
2019-08-25 17:37:05 +0100

    completed calculator logic functionalities

--- js/app.js
+++ js/app.js
@@ -11,14 +11,62 @@ let awaitingValue;
 let evalStringArray = [];
 
 //function that updates display value
-const updateDisplay = (e) => {
+const updateDisplay = e => {
   let numBtnText = e.target.innerText;
   if (displayValue === "0") {
     displayValue = "";
   }
   displayValue += numBtnText;
   input.value = displayValue;
-  console.log(numBtnText);
+};
+
+//function to perform mathematical operations
+const mathOperation = e => {
+  let operatorBtnText = e.target.innerText;
+  switch (operatorBtnText) {
+    case "+":
+      awaitingValue = displayValue;
+      displayValue = "0";
+      input.value = displayValue;
+      evalStringArray.push(awaitingValue);
+      evalStringArray.push("+");
+      break;
+
+    case "-":
+      awaitingValue = displayValue;
+      displayValue = "0";
+      input.value = displayValue;
+      evalStringArray.push(awaitingValue);
+      evalStringArray.push("-");
+      break;
+
+    case "*":
+      awaitingValue = displayValue;
+      displayValue = "0";
+      input.value = displayValue;
+      evalStringArray.push(awaitingValue);
+      evalStringArray.push("*");
+      break;
+
+    case "/":
+      awaitingValue = displayValue;
+      displayValue = "0";
+      input.value = displayValue;
+      evalStringArray.push(awaitingValue);
+      evalStringArray.push("/");
+      break;
+
+    case "=":
+      evalStringArray.push(displayValue);
+      let calculation = eval(evalStringArray.join(" "));
+      displayValue = calculation + "";
+      input.value = displayValue;
+      evalStringArray = [];
+      break;
+
+    default:
+      break;
+  }
 };
 
 //add Eventlisteners to number and operator buttons
@@ -27,7 +75,26 @@ numBtn.forEach(btn => {
 });
 
 operatorBtn.forEach(optrbtn => {
-  optrbtn.addEventListener("click", () => {
-    window.alert("operator button clicked");
-  });
+  optrbtn.addEventListener("click", mathOperation);
 });
+
+//function that clears the input field when the CLR button is clicked
+const clearInput = () => {
+  clearBtn.addEventListener("click", () => {
+    displayValue = "0";
+    evalStringArray = [];
+    input.value = displayValue;
+  });
+};
+clearInput();
+
+//function that inserts decimal point to calculation
+const addDecimal = () => {
+  deciBtn.addEventListener("click", () => {
+    if (!displayValue.includes(".")) {
+      displayValue += ".";
+    }
+    input.value = displayValue;
+  });
+};
+addDecimal();
